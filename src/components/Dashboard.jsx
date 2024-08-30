a334384e83e6ea33c7dcf0674345a242420a749c
GMDLULI <gugulethumdluli98@gmail.com>
2024-08-30 22:27:38 +0200

    fixed sidebar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import '../index.css';  // Assuming you have a separate CSS file
+
 import CreatePatient from './CreatePatient';
 
 const Dashboard = () => {
@@ -9,29 +10,30 @@ const Dashboard = () => {
 
   return (
     <div>
-        <Sidebar/>
+       
         <div className="dashboard-layout">
-        <div className="dashboard-container">
-            <h1>Admin Dashboard</h1>
-            <div className="dashboard-content">
-            <div className="dashboard-item" onClick={() => navigate('/patients')}>
-                <h2>Total Patients</h2>
-                <p>76</p>
+            <Sidebar/>
+            <div className="dashboard-container">
+                <h1>Admin Dashboard</h1>
+                <div className="dashboard-content">
+                <div className="dashboard-item" onClick={() => navigate('/patients')}>
+                    <h2>Total Patients</h2>
+                    <p>76</p>
+                </div>
+                <div className="dashboard-item" onClick={() => navigate('/appointments')}>
+                    <h2>Appointments Today</h2>
+                    <p>5</p>
+                </div>
+                <div className="dashboard-item" onClick={() => navigate('/messages')}>
+                    <h2>Messages</h2>
+                    <p>12</p>
+                </div>
+                <div className="dashboard-item" onClick={() => navigate('/payment-information')}>
+                    <h2>Pending Payments</h2>
+                    <p>3</p>
+                </div>
+                </div>
             </div>
-            <div className="dashboard-item" onClick={() => navigate('/appointments')}>
-                <h2>Appointments Today</h2>
-                <p>5</p>
-            </div>
-            <div className="dashboard-item" onClick={() => navigate('/messages')}>
-                <h2>Messages</h2>
-                <p>12</p>
-            </div>
-            <div className="dashboard-item" onClick={() => navigate('/payment-information')}>
-                <h2>Pending Payments</h2>
-                <p>3</p>
-            </div>
-            </div>
-        </div>
         </div>
       <CreatePatient/>
     </div>
